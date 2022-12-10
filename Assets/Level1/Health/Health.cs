df482b57af299a50886f8d937327eb5d373e784d
VanTinh <tinhngovan410@gmail.com>
2022-12-10 21:10:23 +0700

    Health Player

---
+++ Assets/Level1/Health/Health.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get;  private set; }
+    private Animator anim;
+    private bool dead;
+
+   private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        // spriteRend = GetComponent<SpriteRenderer>();
+    }
+    public void TakeDamage(float _damage)
+    {   
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0 , startingHealth);
+        if(currentHealth> 0 ){
+            // hurt
+            anim.SetTrigger("hurt");
+        }
+      else {
+        if(!dead){
+           anim.SetTrigger("die");
+           GetComponent<model>().enabled = false;
+           dead = true;
+
+        }     
+
+      }
+    } 
+    
+    public void AddHealth(float _value) {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0 , startingHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E)) {
+            TakeDamage(1);
+        }
+        
+    }
+
+}
