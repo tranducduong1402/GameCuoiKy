11d0649ac9f41a8cc2aad8cf121c4993e288d087
duongtd <duong.tranduc@ncc.asia>
2022-12-13 00:19:53 +0700

    duong error

--- Assets/Level1/Health/Health.cs
+++ Assets/Level1/Health/Health.cs
@@ -6,62 +6,93 @@ using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+    private bool invulnerable;
+
+    [Header("Death Sound")]
     [SerializeField] private AudioClip deathSound;
-   private void Awake()
+    [SerializeField] private AudioClip hurtSound;
+
+    private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
-        // spriteRend = GetComponent<SpriteRenderer>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
     public void TakeDamage(float _damage)
     {
+        if (invulnerable) return;
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
         if (currentHealth > 0)
         {
-            // hurt
             anim.SetTrigger("hurt");
+            StartCoroutine(Invunerability());
+            SoundManager.instance.PlaySound(hurtSound);
         }
-<<<<<<< HEAD
-      else {
-        if(!dead){
-           anim.SetTrigger("die");
-           GetComponent<model>().enabled = false;
-           dead = true;
-            SoundManager.instance.PlaySound(deathSound);
-        }     
-=======
         else
         {
             if (!dead)
             {
-                anim.SetTrigger("die");
-                GetComponent<model>().enabled = false;
-                dead = true;
-                SceneManager.LoadScene("Lose");
-            }
+                //Deactivate all attached component classes
+                foreach (Behaviour component in components)
+                    component.enabled = false;
 
+                anim.SetBool("grounded", true);
+                anim.SetTrigger("die");
+
+                dead = true;
+                SoundManager.instance.PlaySound(deathSound);
+            }
         }
     }
->>>>>>> 3f29d76f422cb173b07f5490dd7dc979dae137bf
-
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
-
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Invunerability()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        invulnerable = true;
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
         {
-            TakeDamage(1);
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
         }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+        invulnerable = false;
+    }
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
 
+    //Respawn
+    public void Respawn()
+    {
+        AddHealth(startingHealth);
+        anim.ResetTrigger("die");
+        anim.Play("Idle");
+        StartCoroutine(Invunerability());
+        dead = false;
+
+        //Activate all attached component classes
+        foreach (Behaviour component in components)
+            component.enabled = true;
     }
 
 }
